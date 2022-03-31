766a2f2067e77b517afd2c89f199789742296c75
rli1010 <38045324+rli1010@users.noreply.github.com>
2022-03-31 18:26:20 -0500

    Update analysis.R

--- analysis.R
+++ analysis.R
@@ -6,11 +6,11 @@ suppressMessages(sapply(packages, require, character.only = TRUE))
 
 load("Example.RData")
 
-extraY_var <- "Y1"                    ## biomarker of interest
-T.start = 0                           ## landmark time
-T.end = 5                             ## time point of interest
-M = 50                                 ## number of samples to use from the original MCMC samples, default = 200 for real data analysis
-n.pt <- 50                             ## number of perturbation, default = 200
+extraY_var <- "Y1"                     ## biomarker of interest
+T.start = 0                            ## landmark time
+T.end = 5                              ## time point of interest
+M = 25                                 ## number of samples to use from the original MCMC samples, default = 200 for real data analysis
+n.pt <- 25                             ## number of perturbation, default = 200
 
 cutpoints <- median(longtest[[extraY_var]])  ## threshold to calculate sensitivity and specificity
 
